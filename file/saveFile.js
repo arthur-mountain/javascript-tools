85977c68b1f243728babaa3e6a9e4d1777f135dc
arthur <arthur.oceanus@gmail.com>
2022-06-29 22:43:17 +0800

    add saveFile tools

---
+++ file/saveFile.js
@@ -0,0 +1,13 @@
+function saveFile({ binary, fileName = '', extension = 'csv' }) {
+  const url = window.URL.createObjectURL(new Blob([binary]));
+  const link = dcument.createElement('a')
+  const event = new MouseEvent('click')
+
+  link.href = url
+  link.download = `${fileName}-${new Date().toLocalString()}.${extension}`
+
+  link.dispatchEvent(event)
+  window.URL.revokeObjectURL(url)
+}
+
+export default saveFile;
